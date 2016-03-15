6e69cf5f28fd98af8cee81ac8bdd5213e82ec57c
Wei Zhang <wei.zw1126@gmail.com>
2016-03-15 19:03:21 +1300

    Data storage to database
    by SQLite3

--- parseHtml.py
+++ parseHtml.py
@@ -6,6 +6,7 @@ from email.mime.multipart import MIMEMultipart
 import requests
 from lxml import html
 import ConfigParser
+import re
 
 config = ConfigParser.RawConfigParser()
 config.read("/Users/Wei/pricetracking.cfg")
@@ -36,21 +37,21 @@ product = tree.xpath('//*[@id="column_center"]/div/div[1]/div/div[2]/div/div[2]/
 status = (tree.xpath('//*[@class="input_button_css "]')[0]).text
 specialornot = tree.xpath('//*[@id="productPrices"]/span[1]/text()')
 print specialornot
-if specialornot == ['Special:']:
+if re.search('Special',specialornot[0]):
     price = float(((tree.xpath('//*[@id="productPrices"]/span[4]/text()'))[0].split())[0].strip('$'))
 else:
     price = float(((tree.xpath('//*[@id="productPrices"]/text()'))[4].split())[1].strip('$')) 
 
 if status == 'Sold Out':
-    print 'Sold Out'
+    print "Sold Out"
 elif status == '+ Add To Cart':
     if price < float(config.get('userinfo','expect_price')):
-        print 'Send Mail' 
+        print "Send Mail" 
         sendMail()
     else:
         pass
 else:
     pass
 
-print '%s\n%s\n%s' % (product,status,price)
+print "%s\n%s\n%s" % (product,status,price)
 
--- pricealertgui.py
+++ pricealertgui.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+import sqlite3
 from Tkinter import *
+import tkMessageBox
+import re
 import ConfigParser
 import ttk
 
@@ -9,9 +13,32 @@ def save_entry_to_cfg():
     config.read("/Users/Wei/pricetracking.cfg")
     config.set('userinfo','URL',URL.get())
     config.set('userinfo','receiver',mailaddress.get())
-    config.set('userinfo','expect_price',expectprice.get())
+    
+    #data storage with sqlite3
+    conn = sqlite3.connect('PriceDropAlerts.db')
+    c = conn.cursor()
+    #c.execute('''CREATE TABLE userinfo
+    #(URL,mail address,expect price)
+    #''')
+
+    if re.match('^[-+]?([0-9]+\.[0-9]+|[0-9]+)$',expectprice.get()):
+        #integer may start with naught
+        config.set('userinfo','expect_price',expectprice.get())
+
+        c.execute("INSERT INTO userinfo VALUES ('%s','%s','%s')" % (URL.get(),mailaddress.get(),expectprice.get()))
+        #inscure way
+        #put format arguments into a tuple
+    else:
+        tkMessageBox.showerror("Expect Price","Please input a valid number")
+        print "invalid number."
+
+
     with open("/Users/Wei/pricetracking.cfg",'wb') as configfile:
         config.write(configfile)
+        
+        c.close()
+        conn.commit()
+        conn.close()
 
 root = Tk()
 root.title("Price Drop Alerts")
@@ -35,8 +62,6 @@ mailaddresslabel.grid(column=0,row=1)
 expectpricelabel.grid(column=0,row=2)
 
 URL.grid(column=1,row=0)
-    
-
 mailaddress.grid(column=1,row=1)
 expectprice.grid(column=1,row=2)
 submit.grid(column=0,row=3,columnspan=2)
